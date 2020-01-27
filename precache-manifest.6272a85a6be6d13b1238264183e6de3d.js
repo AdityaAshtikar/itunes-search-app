f2275b93b3402733b7a0c56a6881745abd05d732
AdityaAshtikar <ashtikar.aditya97@gmail.com>
2020-01-28 00:56:07 +0530

    Updates

---
+++ precache-manifest.6272a85a6be6d13b1238264183e6de3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf25b79393e643d2260b943ff7d82d4",
+    "url": "/itunes-search-app/index.html"
+  },
+  {
+    "revision": "080fb7d3a30e9078817d",
+    "url": "/itunes-search-app/static/css/main.4db3ca2c.chunk.css"
+  },
+  {
+    "revision": "8cd3e59c6b3189c72c04",
+    "url": "/itunes-search-app/static/js/2.ecfede83.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/itunes-search-app/static/js/2.ecfede83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "080fb7d3a30e9078817d",
+    "url": "/itunes-search-app/static/js/main.1ced3144.chunk.js"
+  },
+  {
+    "revision": "05b1418c99e9250e6445",
+    "url": "/itunes-search-app/static/js/runtime-main.2bf241d8.js"
+  }
+]);
